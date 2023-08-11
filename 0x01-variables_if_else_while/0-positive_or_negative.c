20b183d08aff67374977a966d61a29a75c655492
omoiyaayo8 <yusuf_a_m@outlook.com>
2023-08-11 14:20:29 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,25 @@
 #include<stdio.h>
 #include <time.h>
 /* more headers goes there */
+
 /* Positive anything is better than negative nothing */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
-	printf("Enter a number: ");
-	scanf("%d",&n);
+	/* Collect integer number from Keyboard and display it */
+
+	printf("Enter a number:\n ");
+	scanf("%d" , &n);
+	if (n>0){
+		printf(" is positive\n");
+	} else if (n==0) {
+		printf("is Zero\n");
+	}
+	else {
+		printf("is negative\n");
+	}
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
