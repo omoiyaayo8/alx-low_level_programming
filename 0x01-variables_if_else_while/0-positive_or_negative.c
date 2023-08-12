90d6c7dbde3a1268cbed8b986d75b75fd7c1cb70
omoiyaayo8 <yusuf_a_m@outlook.com>
2023-08-12 01:48:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,14 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	/* Collect integer number from Keyboard and display it */
        	/*printf("Enter a number: \n");*/
        	scanf("%d", &n);
-       	srand(time(0));
+
 	if (n > 0)
        	{
                 printf("%d is positive\n", n);
