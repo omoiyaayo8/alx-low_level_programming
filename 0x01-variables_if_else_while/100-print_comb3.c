aa4739e9bf0e36914fab5ad2c112e7f44a1d6a2b
omoiyaayo8 <yusuf_a_m@outlook.com>
2023-08-12 04:33:17 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int n;
 
-	for (n = '00'; n <= '99'; n++)
+	for (n = 0; n <= 100; n++)
 	{
-		putchar(n);
+		putchar((n/10)+ '0');
+		putchar((n%10)+ '0');
+		putchar('\n');
 	}
 
-	putchar('\n');
+	/*putchar('\n');*/
 
 	return (0);
 }
