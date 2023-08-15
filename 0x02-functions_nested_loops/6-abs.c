a66f0e8732a9e82e8c9e9f90eb2e3007bcf82fbe
omoiyaayo8 <yusuf_a_m@outlook.com>
2023-08-15 15:55:28 -0700

    absolute_value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Find the absolute value of a number.
+ *@f: The number to find.
+ *
+ *Return: Absolute value of number
+ */
+
+int _abs(int c)
+{
+	if ( c < 0)
+	{
+		int abs_v;
+		abs_v = c * -1;
+		return (abs_v);
+	}
+	return (c);
+}
