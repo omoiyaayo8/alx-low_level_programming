244cdfb8d2e8884118596e1316a48b142b4165cf
omoiyaayo8 <yusuf_a_m@gmail.com>
2023-10-25 21:20:09 +0100

    4-pow_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,11 @@
 
 int factorial(int n)
 {
-		int next;
-		if (n == 0)
-			return (1);
-		else if (n < 0)
-			return (-1);
-		next = factorial(n - 1);
-		return (n * next);
+	int next;
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	next = factorial(n - 1);
+	return (n * next);
 }
