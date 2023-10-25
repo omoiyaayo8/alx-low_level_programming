ccc7f79133c18f87ad4d79ff3cf85e3593184a25
omoiyaayo8 <yusuf_a_m@gmail.com>
2023-10-25 21:15:13 +0100

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,12 @@
 
 int _strlen_recursion(char *s)
 {
-		int len = 0;
-		
-		if (*s != '\0')
-		{
-			len++;
-			len += _strlen_recursion(s + 1);
-		}
-		return (len);
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
 }
